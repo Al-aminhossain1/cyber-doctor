1d2e80f1e0da84915c9c3e94e41d3b8cd7a82925
Al-aminhossain1 <md.al.aminhossain1785@gmail.com>
2022-04-17 09:16:52 +0600

    project initialize

---
+++ src/Component/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import img from '../../images/not found.png'
+
+const NotFound = () => {
+    return (
+        <div className='mt-100px'>
+            <img className='w-50 mt-5' src={img} alt="" />
+        </div>
+    );
+};
+
+export default NotFound;
