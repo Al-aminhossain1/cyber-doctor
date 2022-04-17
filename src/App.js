c80bd8c14ed7f08825394577640744f201cbce5f
Al-aminhossain1 <md.al.aminhossain1785@gmail.com>
2022-04-17 23:30:27 +0600

    signup page design with create user

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Header from './Component/Header/Header';
 import { Route, Routes } from 'react-router-dom';
@@ -8,6 +8,7 @@ import Footer from './Component/Footer/Footer';
 import Checkout from './Component/Checkout/Checkout';
 import Login from './Component/Login/Login';
 import RequireAuth from './Component/RequireAuth/RequireAuth';
+import SignUp from './Component/SignUp/SignUp';
 
 function App() {
   return (
@@ -17,7 +18,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
-        <Route path='/' element={<p></p>}></Route>
+        <Route path='/signup' element={<SignUp></SignUp>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/checkout' element={<RequireAuth>
           <Checkout></Checkout>
--- src/Component/SignUp/SignUp.js
+++ src/Component/SignUp/SignUp.js
@@ -1,35 +1,46 @@
+import { Button } from 'react-bootstrap';
 import React, { useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
-import { Link, Navigate, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
+import { Link } from 'react-router-dom';
 
-const Login = () => {
-    const [login, setLogin] = useState('false')
+const SignUp = () => {
+
+
+
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
     const [
         createUserWithEmailAndPassword,
         user,
         loading,
         error,
     ] = useCreateUserWithEmailAndPassword(auth);
-    // const navigate = useNavigate()
-    // const handelRegister = () => {
-    //     navigate('/signup')
-    // }
+
+    const handelEmailBlur = event => {
+        setEmail(event.target.value);
+    }
+    const handelPasswordBlur = event => {
+        setPassword(event.target.value);
+    }
+    const handelConfirmPasswordBlur = event => {
+        setConfirmPassword(event.target.value);
+    }
 
     const formSubmiteButton = event => {
         event.preventDefault()
         createUserWithEmailAndPassword(email, password)
+        console.log(user);
     }
     return (
         <div>
-            <h3 className='text-center mt-2'>{login ? "Log In" : "Resister"}</h3>
+            <h3 className='text-center mt-2'>Resister</h3>
             <Form onSubmit={formSubmiteButton} className='w-50 mx-auto'>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" required />
+                    <Form.Control onBlur={handelEmailBlur} type="email" placeholder="Enter email" required />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -37,19 +48,21 @@ const Login = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" required />
+                    <Form.Control onBlur={handelPasswordBlur} type="password" placeholder="Password" required />
                 </Form.Group>
-                {!login && <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Confirm Password</Form.Label>
-                    <Form.Control type="password" placeholder="Confirm Password" required />
-                </Form.Group>}
-                <p>New to Cyber doctor? <Link className='text-decoration-none' to='/signup'>create an account</Link></p>
+                    <Form.Control onBlur={handelConfirmPasswordBlur} type="password" placeholder="Confirm-Password" required />
+                </Form.Group>
+                <p>Already have an account? <Link className='text-decoration-none' to='/login'>Please login</Link></p>
                 <Button variant="primary" type="submit">
-                    {login ? 'Login' : 'Resister'}
+                    Register
                 </Button>
+
             </Form>
+
         </div>
     );
 };
 
-export default Login;
+export default SignUp;
