937fc6d010386d9dec4166b31bf6e4546d515cef
Al-aminhossain1 <md.al.aminhossain1785@gmail.com>
2022-04-19 22:46:14 +0600

    added about me in about page

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import hacker from '../../images/cyber1.png'
 
 const About = () => {
     return (
         <div>
             <h1 className='text-center'>welcome to know about me!!!</h1>
             <div className='container row '>
-                <div className=' m-5 col col-sm-12 col-lg-6 p-5'>
-                    <div>
-                        <h3>hey,
-                            i am md Shahenoor rahman
-                        </h3>
-                        <p>i want to be a good web developer</p>
-                        <p className='w-100 pe-5'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam corrupti repellendus ratione dolor, suscipit hic labore dolorum aut nam odit obcaecati dicta possimus, enim ut magni. At ullam hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa libero distinctio veritatis numquam. Repellendus totam, quod suscipit recusandae et sunt architecto! Corrupti ullam, animi eos assumenda numquam non voluptate iste sint nulla delectus perferendis. Quis suscipit earum sunt corporis? Quis corporis atque nisi mollitia? Officiis consequuntur nobis maiores a molestias!</p>
-                    </div>
-                    <div>
-                        <img className='w-100' src={hacker} alt="" />
-                    </div>
+                <div>
+                    <h3>hey <br />
+                        i am md Al Amin Hossain
+                    </h3>
+                    <p>I am a web developer and cyber security specialist & penetration-tester. i also provide web design, malware remove and hacked website recovery of this service.</p>
+                    <p className='w-100 pe-5'></p>
                 </div>
+                <div>
+                    <h3>My Goal</h3>
+                    <p> I Want to be a good web developer and my dream is create an fantastic website. where is the site i provide my service. and i started my carrier in Tech field.</p>
+                </div>
+                <div>
+                    <h3>The way I hard work to achieve my goal</h3>
+                    <p> I work hard to achieve my goals. That's why I enroll in the programming hero web development course and practice for 8-10 hours every day according to Jhankar Vaiya. I watch tutorials from different youtube channels. I read the documentation from Google and practice accordingly.</p>
+                </div>
+
             </div>
         </div>
     );
